85864d20a9f16d18f5060616ffd3a9321f69d91c
Ryan Morr <rm.morr@gmail.com>
2024-10-22 12:15:45 -0400

    Build v0.2.0

--- dist/esm/amble.js
+++ dist/esm/amble.js
@@ -1,2 +1,2 @@
-/*! @ryanmorr/amble v0.1.3 | https://github.com/ryanmorr/amble */
+/*! @ryanmorr/amble v0.2.0 | https://github.com/ryanmorr/amble */
 const e=/([^{};]*)([;{}])/g,r=/(\r\n|\r|\n)+/g,c=/\t/g,l=/\s{2,}/g,n=/\/\*[\W\w]*?\*\//g,t=/\s*([:;{}])\s*/g,a=/\};+/g,g=/([^:;{}])}/g;function p(p,o){p=function(e){return e.replace(r," ").replace(c," ").replace(l," ").replace(n,"").trim().replace(t,"$1").replace(a,"}").replace(g,"$1;}")}(p),e.lastIndex=0;for(let r;null!=(r=e.exec(p));)o(r[1],r[2])}export{p as walk};
